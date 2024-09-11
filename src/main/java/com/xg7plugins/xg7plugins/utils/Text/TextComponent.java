e4c6a86e2b344ef738785a2655683661951dbaef
davis <davisonic2010@gmail.com>
2024-09-11 19:22:59 -0300

    wait

--- src/main/java/com/xg7plugins/xg7plugins/utils/Text/TextComponent.java
+++ src/main/java/com/xg7plugins/xg7plugins/utils/Text/TextComponent.java
@@ -1,15 +1,30 @@
 package com.xg7plugins.xg7plugins.utils.Text;
 
+import lombok.Getter;
 import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.entity.Player;
 
+import java.util.regex.Pattern;
+
+@Getter
 public class TextComponent {
 
-    private BaseComponent[] components;
+    private Pattern clickRegex = Pattern.compile("\\[CLICK ]");
+
     private String rawText;
 
     public TextComponent(String text) {
+        //Vou fazer o negocio de tirar as tags depois
+        this.rawText = text;
+    }
 
 
+    public void send(Player player) {
+        //Traduzir os componentes
+        player.spigot().sendMessage(components);
+    }
+
+    public void translateComponents() {
 
     }
 
