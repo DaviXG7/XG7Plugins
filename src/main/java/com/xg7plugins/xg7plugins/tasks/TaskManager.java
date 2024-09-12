0f225e290ca94006f74e33a94aeb2c3d7aad8a86
davis <davisonic2010@gmail.com>
2024-09-12 20:24:35 -0300

    wait

--- src/main/java/com/xg7plugins/xg7plugins/tasks/TaskManager.java
+++ src/main/java/com/xg7plugins/xg7plugins/tasks/TaskManager.java
@@ -1,29 +1,44 @@
 package com.xg7plugins.xg7plugins.tasks;
 
 import com.xg7plugins.xg7plugins.XG7Plugins;
+import com.xg7plugins.xg7plugins.boot.Plugin;
 import com.xg7plugins.xg7plugins.data.config.Config;
-import org.bukkit.Bukkit;
-import org.bukkit.scheduler.BukkitTask;
 
-import java.util.concurrent.ExecutorService;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class TaskManager {
 
     protected ScheduledExecutorService executor;
 
-    private BukkitTask mainScheduler;
+    private final Map<Plugin, HashMap<UUID, ScheduledFuture<?>>> tasksRunning = new HashMap<>();
 
     public TaskManager(XG7Plugins plugin) {
         Config config = plugin.getConfigsManager().getConfig("config");
 
         executor = (ScheduledExecutorService) Executors.newFixedThreadPool(config.get("task-threads"));
 
-        Thread thread = new Thread();
-
-
     }
+
+    public addRepeatingTask(Plugin plugin, Runnable runnable, long delay) {
+        tasksRunning.putIfAbsent(plugin, new HashMap<>());
+        tasksRunning.get(plugin).put(UUID.randomUUID(), executor.schedule(runnable, delay, TimeUnit.MILLISECONDS));
+    }
+    public addCooldownTask(Plugin plugin, Runnable runnable, int seconds) {
+        tasksRunning.putIfAbsent(plugin, new HashMap<>());
+
+        UUID taskId = UUID.randomUUID();
+
+        tasksRunning.get(plugin).put(taskId, executor.schedule(runnable, seconds, TimeUnit.SECONDS));
+        executor.submit(() -> {
+            tasksRunning.get(plugin).get(taskId).cancel(false);
+            tasksRunning.get(plugin).remove(taskId);
+        });
+    }
+
 
 
 
